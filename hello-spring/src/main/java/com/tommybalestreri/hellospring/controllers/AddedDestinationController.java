e8cd32a0e2bee9d7b83165913963de0a2ca15055
Tommy Balestreri <tcbstl@gmail.com>
2020-04-19 18:38:23 -0500

    Still trying some things - improving functionality

---
+++ hello-spring/src/main/java/com/tommybalestreri/hellospring/controllers/AddedDestinationController.java
@@ -0,0 +1,33 @@
+//package com.tommybalestreri.hellospring.controllers;
+//
+//import com.tommybalestreri.hellospring.data.DestinationRepository;
+//import com.tommybalestreri.hellospring.data.UserRepository;
+//import com.tommybalestreri.hellospring.models.User;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//
+//@Controller
+//public class AddedDestinationController {
+//
+//    @Autowired
+//    private DestinationRepository destinationRepository;
+//
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    @GetMapping("addeddestination")
+//    public String processDestinationPageAfter(User user, Model model){
+//        model.addAttribute("users", userRepository.findAll());
+//        return "addeddestination";
+//    }
+//
+//    @PostMapping("addeddestination")
+//    public String submitDestinationPage(User user, Model model){
+//        model.addAttribute("users", userRepository.findAll());
+//        return "addeddestination";
+//    }
+//
+//}
