f0ec973db66e8148982d8d5fd4e2e443549b9b4b
Tommy Balestreri <tcbstl@gmail.com>
2020-05-03 15:12:07 -0500

    Added some login and homepage styling

--- hello-spring/src/main/java/com/tommybalestreri/hellospring/controllers/WebSecurityConfig.java
+++ hello-spring/src/main/java/com/tommybalestreri/hellospring/controllers/WebSecurityConfig.java
@@ -31,6 +31,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/register").permitAll()
                 .antMatchers("/users/addNew").permitAll()
+                .antMatchers("/*.css").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
