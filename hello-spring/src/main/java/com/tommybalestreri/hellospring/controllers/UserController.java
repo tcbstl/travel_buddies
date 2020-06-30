0bf71d675d82c71ccf045c5476430295e2af1d40
Tommy Balestreri <tcbstl@gmail.com>
2020-06-29 21:31:28 -0500

    Validation added to registration page

--- hello-spring/src/main/java/com/tommybalestreri/hellospring/controllers/UserController.java
+++ hello-spring/src/main/java/com/tommybalestreri/hellospring/controllers/UserController.java
@@ -5,10 +5,12 @@ import com.tommybalestreri.hellospring.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.servlet.view.RedirectView;
+
+import javax.validation.Valid;
 
 @Controller
 public class UserController {
@@ -20,13 +22,44 @@ public class UserController {
         return "user";
     }
 
-    @PostMapping(value="users/addNew")
-    public RedirectView addNew(User user, RedirectAttributes redir) {
-        userService.save(user);
-        RedirectView redirectView= new RedirectView("/login", true);
-            redir.addFlashAttribute("message", "Registration successful");
-            return redirectView;
+    @GetMapping("register")
+    public String register(Model model) {
+        return "register";
+    }
+
+//This works!
+//    @PostMapping(value="users/addNew")
+//    public RedirectView addNew(User user, RedirectAttributes redir) {
+//        userService.save(user);
+//        RedirectView redirectView= new RedirectView("/login", true);
+//            redir.addFlashAttribute("message", "Registration successful");
+//            return redirectView;
+//        }
+
+    //Trying something new
+    @PostMapping(value="register")
+    public String addNew(@ModelAttribute @Valid User user,
+                               Errors errors, Model model) {
+
+//        if(errors.hasErrors()){
+//            RedirectView redirectView= new RedirectView("/register", true);
+//            redir.addFlashAttribute("message", "Registration unsuccessful");
+////            model.addAttribute("message", "Registration unsuccessful");
+//            return redirectView;
+//        }
+
+        if(errors.hasErrors()){
+//            RedirectView redirectView= new RedirectView("/register", true);
+//            redir.addFlashAttribute("message", "Registration unsuccessful");
+            model.addAttribute("errorMsg", "Registration unsuccessful");
+            return "register";
         }
+
+        userService.save(user);
+//        RedirectView redirectView= new RedirectView("/login", true);
+//        redir.addFlashAttribute("message", "Registration successful");
+        return "login";
+    }
 
     }
 
